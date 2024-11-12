1e02d32561e3a677e7a7c510818dd49a3bf581b0
Rasel Chowdhury <rasel.chowdhury0001@gmail.com>
2024-11-13 00:21:15 +0600

    hero slider section Ui done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Home from './Components/Home'
 import Navbar from './Components/Navbar'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
        <Navbar/>
+       <Home/>
     </>
   )
 }
