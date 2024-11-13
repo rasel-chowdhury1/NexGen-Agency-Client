c8ca739b72759712fbc3ed6d4fb64bfbed9a92cc
Rasel Chowdhury <rasel.chowdhury0001@gmail.com>
2024-11-13 21:29:57 +0600

    Newsletter section ui done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from './Components/About'
 import Blog from './Components/Blog'
 import Hero from './Components/Hero'
 import Navbar from './Components/Navbar'
+import NewsLetter from './Components/NewsLetter'
 import Product from './Components/Product'
 import Services from './Components/Services'
 
@@ -17,6 +18,7 @@ function App() {
        <About/>
        <Product/>
        <Blog/>
+       <NewsLetter/>
     </>
   )
 }
---
+++ src/Components/NewsLetter.jsx
@@ -0,0 +1,19 @@
+import { FaArrowRightLong } from "react-icons/fa6";
+
+const NewsLetter = () => {
+    return (
+        <div className="px-4 lg:px-14 max-w-screen-2xl mx-auto bg-newtralSilver py-16">
+            <div>
+                <div className="text-center">
+                    <h2 className="lg:text-5xl text-3xl text-neutral-800 font-semibold mb-6 lg:leading-snug">Pellentesque suscipit <br/> fringilla libero eu.</h2>
+                    <div className="flex justify-center lg:w-2/5 mx-auto" >
+                        <button className="btn-primary flex items-center justify-center gap-2">Get a Demo <FaArrowRightLong  /></button>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NewsLetter;
